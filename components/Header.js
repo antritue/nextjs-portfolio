fe179c17a17fe484fbba3125da2e422a16ba8069
antritue <antritue@gmail.com>
2022-11-16 01:50:03 +0700

    minor fix and update meta tags

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className='header center'>
-      <h3>Portfolio</h3>
+      <h3>
+        <Link href='/' className='link'>
+          Portfolio
+        </Link>
+      </h3>
       <Navbar />
     </header>
   );
